e1c987c0cdac72ecaf1caee46a3cf295fc1f2b52
paul-ww <44506902+paul-ww@users.noreply.github.com>
2021-12-15 16:58:11 +0100

    Refactor filtering

--- pipeline/02_01_filter_data.py
+++ pipeline/02_01_filter_data.py
@@ -1,34 +1,18 @@
-from datetime import datetime
-import json
-from abc import ABC, abstractmethod
-import numpy as np
-from tqdm import tqdm
-from utils.tweets import clean_tweet, read_tweets
-import math
-import time
 import hashlib
+import json
 import os
+from typing import Optional
 
-# DATASET = 'geoengineering'
-DATASET = 'climate'
-SOURCE_FILE = f'data/{DATASET}/tweets_clean.jsonl'
-
-LIMIT = 10000
-ONLY_EN = True
-MIN_TOKENS = 4
-MAX_HASHTAGS = 5
-
-RELEVANCE_FILE = f'data/{DATASET}/tweets_relevant_{ONLY_EN}_{MIN_TOKENS}_{MAX_HASHTAGS}.txt'
-IRRELEVANCE_FILE = f'data/{DATASET}/tweets_irrelevant_{ONLY_EN}_{MIN_TOKENS}_{MAX_HASHTAGS}.txt'
-TARGET_FILE = f'data/{DATASET}/tweets_filtered_{LIMIT}.jsonl'
+from tqdm import tqdm
+from utils.io import exit_if_exists
 
 
-def is_relevant(tweet):
-    if ONLY_EN and tweet['lang'] != 'en':
+def is_relevant(tweet, only_en, min_tokens, max_hashtags):
+    if only_en and tweet['lang'] != 'en':
         return False
     if tweet['text'] is None:
         return False
-    if tweet['meta']['n_tokens_raw'] <= MIN_TOKENS and tweet['meta']['n_hashtags'] >= MAX_HASHTAGS:
+    if tweet['meta']['n_tokens_raw'] <= min_tokens and tweet['meta']['n_hashtags'] >= max_hashtags:
         return False
     return True
 
@@ -37,28 +21,46 @@ def get_hash(tweet):
     return hashlib.md5(f'{tweet["author_id"]}|{tweet["clean_text"].lower()}'.encode()).digest()
 
 
-if __name__ == '__main__':
-    if os.path.exists(TARGET_FILE):
-        print(f'The file {TARGET_FILE} already exists. If you are sure you want to proceed, delete it first.')
-        exit(1)
+def filter_dataset(
+    dataset: str,
+    limit: int,
+    only_en: bool,
+    min_tokens: int,
+    max_hashtags: int,
+    relevance_f: Optional[str] = None,
+    irrelevance_f: Optional[str] = None,
+    source_f: Optional[str] = None,
+    target_f: Optional[str] = None,
+):
 
-    if not os.path.exists(RELEVANCE_FILE):
+    if source_f is None:
+        source_f = f"data/{dataset}/tweets_raw.jsonl"
+    if target_f is None:
+        target_f = f"data/{dataset}/tweets_filtered_{limit}.jsonl"
+    if relevance_f is None:
+        relevance_f = f"data/{dataset}/tweets_relevant_{only_en}_{min_tokens}_{max_hashtags}.txt"
+    if irrelevance_f is None:
+        irrelevance_f= f'data/{dataset}/tweets_irrelevant_{only_en}_{min_tokens}_{max_hashtags}.txt'
+
+    exit_if_exists(target_f)
+
+    if not os.path.exists(relevance_f):
         num_lines = 0
         n_duplicates = 0
         n_irrelevant = 0
         print('Filter and remove duplicates...')
-        with open(SOURCE_FILE, 'r') as f_in, \
-                open(RELEVANCE_FILE, 'w') as f_rel_out, \
-                open(IRRELEVANCE_FILE, 'w') as f_irrel_out:
+        with open(source_f, 'r') as f_in, \
+                open(relevance_f, 'w') as f_rel_out, \
+                open(irrelevance_f, 'w') as f_irrel_out:
             hashes = set()
             for line_i, line in tqdm(enumerate(f_in)):
                 tweet_o = json.loads(line)
                 num_lines += 1
 
-                is_en = (not ONLY_EN) or (ONLY_EN and tweet_o['lang'] != 'en')
+                is_en = (not only_en) or (only_en and tweet_o['lang'] != 'en')
                 has_text = tweet_o['text'] is not None
-                has_min_tokens = tweet_o['meta']['n_tokens_raw'] >= MIN_TOKENS
-                has_max_hashtags = tweet_o['meta']['n_hashtags'] <= MAX_HASHTAGS
+                has_min_tokens = tweet_o['meta']['n_tokens_raw'] >= min_tokens
+                has_max_hashtags = tweet_o['meta']['n_hashtags'] <= max_hashtags
 
                 # if is_relevant(tweet_o):
                 if is_en and has_text and has_min_tokens and has_max_hashtags:
@@ -81,15 +83,15 @@ if __name__ == '__main__':
 
         N_LINES = num_lines - n_duplicates - n_irrelevant
     else:
-        print(f'I\'m using the already existing relevance file {RELEVANCE_FILE}!')
-        with open(RELEVANCE_FILE) as f:
+        print(f'I\'m using the already existing relevance file {relevance_f}!')
+        with open(relevance_f) as f:
             N_LINES = sum(1 for _ in f)
 
-    SKIP_LINES = max(int(N_LINES / LIMIT), 1)
+    SKIP_LINES = max(int(N_LINES / limit), 1)
 
-    print(f'Aiming to reduce size of the dataset from {N_LINES:,} to {LIMIT:,} '
+    print(f'Aiming to reduce size of the dataset from {N_LINES:,} to {limit:,} '
           f'by skipping {SKIP_LINES} relevant non-duplicate tweets.')
-    with open(SOURCE_FILE) as f_source, open(RELEVANCE_FILE) as f_rel, open(TARGET_FILE, 'w') as f_out:
+    with open(source_f) as f_source, open(relevance_f) as f_rel, open(target_f, 'w') as f_out:
         line_source = 0
         line_rel = 0
 
@@ -106,3 +108,13 @@ if __name__ == '__main__':
             else:  # skip / ignore line
                 next(f_source)
             line_rel += 1
+
+if __name__ == '__main__':
+
+    filter_dataset(
+        dataset="climate",  # 'geoengineering'
+        limit=10000,
+        only_en=True,
+        min_tokens=4,
+        max_hashtags=5
+    )
