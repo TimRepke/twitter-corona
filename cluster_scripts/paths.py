7b517789097bcf7d27103d071342885e76d3ddaf
paul-ww <44506902+paul-ww@users.noreply.github.com>
2021-12-17 14:46:27 +0100

    Use dataset in cluster paths

--- cluster_scripts/paths.py
+++ cluster_scripts/paths.py
@@ -9,13 +9,21 @@ path_like = Union[str, Path]
 
 def get_paths_from_environment():
     load_dotenv(Path(__file__).parent / ".env")
-    work_dir = Path(os.getenv("WORKDIR"))
-    source_file = Path(os.getenv("SOURCE_FILE"))
-    output_dir = Path(os.getenv("OUTPUT_SUBDIR"))
-    model_cache_dir = Path(os.getenv("MODEL_CACHE_SUBDIR"))
+    work_dir = Path(os.environ["WORKDIR"])
+    dataset = os.environ["DATASET"]
+    source_file = Path(os.environ["SOURCE_FILE"])
+    model_cache_dir = Path(os.environ["CACHE_SUBDIR"])
+    file_embeddings = os.environ["TARGET_E"]
+    file_classification = os.environ["TARGET_C"]
     return {
         "work": work_dir,
-        "source": work_dir / source_file,
-        "output": work_dir / output_dir,
+        "source": work_dir / dataset / source_file,
+        "embeddings": work_dir / dataset / file_embeddings,
+        "classification": work_dir / dataset / file_classification,
         "cache": work_dir / model_cache_dir,
     }
+
+
+def check_for_cache_dir(cache_dir: path_like):
+    if not Path(cache_dir).exists():
+        raise FileNotFoundError(f"Could not find model cache directory {cache_dir}")
