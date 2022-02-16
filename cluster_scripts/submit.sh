96f1710eb6494e2d9616db0909ca1d414bc0b902
timrepke <timrepke@pik-potsdam.de>
2022-02-16 21:22:39 +0100

    remove deprecated cluster scripts

--- cluster_scripts/submit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# load environment variables from file
-export $(grep -v '^#' .env | xargs)
-
-# submit the job
-sbatch --mail-user=$MAIL_USER --workdir=$WORKDIR --job-name="${TASK}_${SOURCE_FILE}" slurm_job.sh
